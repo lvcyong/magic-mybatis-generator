9e9ce0ea8a2c8030e65fc06779116aabffdd1277
lvcyong <lvcy@golden-soft.com>
2017-09-26 11:19:34 +0800

    init commit

---
+++ magic-mybatis-generator-test/src/main/java/com/magic/base/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.magic.base.entity;
+
+import java.io.Serializable;
+
+/**
+ * <br>Filename:    BaseEntity  <br>
+ * Description:
+ * <br>Copyright:   Copyright (c) 2016-2017 All Rights Reserved.<br>
+ * Company:     golden-soft.com Inc.<br>
+ * author:     lvcyong <br>
+ * version:    1.0  <br>
+ * Create at:   2017-09-26 11:14 <br>
+ * <p>
+ * Modification History:
+ * <br>Date    &nbsp;     Author      Version     Description  <br>
+ * ------------------------------------------------------------------  <br>
+ * 2017-09-26    lvcyong      1.0         1.0 Version  <br>
+ */
+public class BaseEntity implements Serializable {
+}
---
+++ magic-mybatis-generator-test/src/main/java/com/magic/base/mapper/BaseMapper.java
@@ -0,0 +1,18 @@
+package com.magic.base.mapper;
+
+/**
+ * <br>Filename:    BaseMapper  <br>
+ * Description:
+ * <br>Copyright:   Copyright (c) 2016-2017 All Rights Reserved.<br>
+ * Company:     golden-soft.com Inc.<br>
+ * author:     lvcyong <br>
+ * version:    1.0  <br>
+ * Create at:   2017-09-26 11:19 <br>
+ * <p>
+ * Modification History:
+ * <br>Date    &nbsp;     Author      Version     Description  <br>
+ * ------------------------------------------------------------------  <br>
+ * 2017-09-26    lvcyong      1.0         1.0 Version  <br>
+ */
+public interface BaseMapper {
+}
