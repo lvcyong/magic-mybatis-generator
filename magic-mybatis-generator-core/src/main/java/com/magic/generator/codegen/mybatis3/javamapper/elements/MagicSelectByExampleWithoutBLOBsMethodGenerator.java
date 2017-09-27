8341f9f297ba299be601ca132ef046cb51c0772c
lvcyong <lvcy@golden-soft.com>
2017-09-27 15:05:04 +0800

    init commit

---
+++ magic-mybatis-generator-core/src/main/java/com/magic/generator/codegen/mybatis3/javamapper/elements/MagicSelectByExampleWithoutBLOBsMethodGenerator.java
@@ -0,0 +1,18 @@
+package com.magic.generator.codegen.mybatis3.javamapper.elements;
+
+/**
+ * <br>Filename:    MagicSelectByExampleWithoutBLOBsMethodGenerator  <br>
+ * Description:
+ * <br>Copyright:   Copyright (c) 2016-2017 All Rights Reserved.<br>
+ * Company:     golden-soft.com Inc.<br>
+ * author:     lvcyong <br>
+ * version:    1.0  <br>
+ * Create at:   2017-09-27 15:02 <br>
+ * <p>
+ * Modification History:
+ * <br>Date    &nbsp;     Author      Version     Description  <br>
+ * ------------------------------------------------------------------  <br>
+ * 2017-09-27    lvcyong      1.0         1.0 Version  <br>
+ */
+public class MagicSelectByExampleWithoutBLOBsMethodGenerator {
+}
